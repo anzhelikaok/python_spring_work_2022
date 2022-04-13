13d175be5a1151488509b458fe18c2068329a675
anzhelikaok <anzhelika_ok@bk.ru>
2022-04-13 17:14:39 +0300

    Created task 10 (lesson 3)

--- task5.py
+++
@@ -1,8 +0,0 @@
-a = float(input('A'))
-b = float(input('B'))
-c = float(input('C'))
-print('AC: ' + str(a + c))
-print('BC: ' + str(b + c))
-
-
-
---
+++ unit_one/lesson__2/task6.py
@@ -0,0 +1,4 @@
+a = float(input('a'))
+b = float(input('b'))
+x = float(input('x'))
+print(a*x + b)
